d336debf9f177222fd32137423105975dbb16169
ashish <ashishsh6492@gmail.com>
2021-02-08 01:49:59 +0530

    second Init

--- src/test/java/test/testcase.java
+++ src/test/java/test/testcase.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -26,7 +27,7 @@ void Login()
 {
 	
 	System.out.print(driver.getTitle());
-	
+	driver.findElement(By.id("identifierId")).sendKeys("as@gmil.com");
 }
 
 @AfterTest
